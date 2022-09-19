19af09f12ebca1b5e2384cd870fa686a1496600f
rendegosling <renlesterdg@gmail.com>
2022-09-19 10:49:48 +0800

    :sparkles: use chalk for logging

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import http from 'http';
 import mongoose from 'mongoose';
 import { config } from './config/config';
+import Logging from './library/Logging';
 
 const router = express();
 
 /** Connec Mongo */
 mongoose.connect(config.mongo.url, { retryWrites: true, w: 'majority' })
-.then(() => { console.log('connected')})
-.catch(error => {console.log(error)})
+.then(() => { 
+    Logging.info('Connected to mongoDB')
+})
+.catch(error => {
+    Logging.error('Unable to connect:')
+    Logging.error(error);
+})
