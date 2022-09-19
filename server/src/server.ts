d0ca7192993e6fdee6d7e3f3411fe2a89c298a8b
rendegosling <renlesterdg@gmail.com>
2022-09-20 00:19:15 +0800

    :sparkles: health check error handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,8 +10,44 @@ const router = express();
 mongoose.connect(config.mongo.url, { retryWrites: true, w: 'majority' })
 .then(() => { 
     Logging.info('Connected to mongoDB')
+    StartServer()
 })
 .catch(error => {
     Logging.error('Unable to connect:')
     Logging.error(error);
 })
+
+const StartServer = () => {
+    router.use((req, res, next ) => {
+        Logging.info(`Incoming  -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}] `)
+        next()
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+        next();
+    });
+
+    /** Routes */
+
+    /** Healthcheck */
+    router.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong' }));
+
+    /** Error handling */
+    router.use((req, res, next) => {
+        const error = new Error("not Found");
+        Logging.error(error);
+
+        return res.status(404).json({ message: error.message});
+    });
+
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}.`));
+}
